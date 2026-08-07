ae8590799d02aedb8ce34cfea43b239869a1b5e1
Muyiwa Oladimeji <muyiwaoladimeji0@gmail.com>
2026-08-07 22:40:42 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -56,6 +56,7 @@ self.addEventListener("push", (event) => {
     self.registration.showNotification(data.title || "MyUTME", {
       body: data.body || "",
       icon: "./icon-192.png",
+      badge: "./icon-badge-96.png",
       data: data.url || "/",
     })
   );
